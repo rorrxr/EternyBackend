529e188e3cbba4a6c468e2836aa6ae1e908103b3
MinJu Kim <rorrxr@naver.com>
2025-05-07 01:49:49 +0900

    refactor : User Table 어노테이션 추가

--- backend/src/main/java/com/company/eterny/user/entity/User.java
+++ backend/src/main/java/com/company/eterny/user/entity/User.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
