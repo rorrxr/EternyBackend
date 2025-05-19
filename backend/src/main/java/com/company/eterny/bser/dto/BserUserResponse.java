166e0a66a909f7db3cb650fa9005d644a6ad90d0
rorrxr <rorrxr@naver.com>
2025-05-19 10:24:38 +0900

    refactor : UserResponse 리팩토링

--- backend/src/main/java/com/company/eterny/bser/dto/BserUserResponse.java
+++ backend/src/main/java/com/company/eterny/bser/dto/BserUserResponse.java
@@ -1,19 +1,27 @@
 package com.company.eterny.bser.dto;
 
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class BserUserResponse<T> {
     private int code;
     private String message;
-    // BSER 리턴의 "user": { ... } 부분을 매핑
-    @JsonProperty("user")
-    private T user;
+
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+    private List<T> user;
+
+//    private DataWrapper<T> data;
+//
+//    public static class DataWrapper<T> {
+//        @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+//        private List<T> user;
+//        // getter / setter
+//    }
 }
